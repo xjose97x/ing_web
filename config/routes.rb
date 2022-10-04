9bbb1c43c2df52eac4d797733a2c6423caa2b0c8
Jose Escudero <joseignacioescudero@gmail.com>
2022-10-03 20:28:42 -0500

    Configure authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: ""
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+
+  get "log_out", to: "home#log_out"
 
   resources :users
 end
